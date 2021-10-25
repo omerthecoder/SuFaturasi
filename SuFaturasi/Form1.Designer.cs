bcfce18913ddedf97081b59e0f19885ebac45876
Omer Salih Kanra <okanra@uaa.k12.tr>
2021-10-25 10:57:46 +0300

    Add project files.

---
+++ SuFaturasi/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace SuFaturasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.radioUmumi = new System.Windows.Forms.RadioButton();
+            this.lstAdSoyad = new System.Windows.Forms.ListBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstKSuMiktari = new System.Windows.Forms.ListBox();
+            this.lstFaturaTutari = new System.Windows.Forms.ListBox();
+            this.radioMesken = new System.Windows.Forms.RadioButton();
+            this.txtSonIndex = new System.Windows.Forms.TextBox();
+            this.txtIlkIndex = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı Soyadı:";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(110, 19);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(382, 20);
+            this.txtAdSoyad.TabIndex = 1;
+            // 
+            // radioUmumi
+            // 
+            this.radioUmumi.AutoSize = true;
+            this.radioUmumi.Location = new System.Drawing.Point(6, 19);
+            this.radioUmumi.Name = "radioUmumi";
+            this.radioUmumi.Size = new System.Drawing.Size(57, 17);
+            this.radioUmumi.TabIndex = 2;
+            this.radioUmumi.TabStop = true;
+            this.radioUmumi.Text = "Umumi";
+            this.radioUmumi.UseVisualStyleBackColor = true;
+            // 
+            // lstAdSoyad
+            // 
+            this.lstAdSoyad.FormattingEnabled = true;
+            this.lstAdSoyad.Location = new System.Drawing.Point(44, 239);
+            this.lstAdSoyad.Name = "lstAdSoyad";
+            this.lstAdSoyad.Size = new System.Drawing.Size(133, 199);
+            this.lstAdSoyad.TabIndex = 4;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(359, 154);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(133, 23);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Son Index:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "İlk Index";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioMesken);
+            this.groupBox1.Controls.Add(this.radioUmumi);
+            this.groupBox1.Location = new System.Drawing.Point(110, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 51);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kullanım Yeri";
+            // 
+            // lstKSuMiktari
+            // 
+            this.lstKSuMiktari.FormattingEnabled = true;
+            this.lstKSuMiktari.Location = new System.Drawing.Point(197, 239);
+            this.lstKSuMiktari.Name = "lstKSuMiktari";
+            this.lstKSuMiktari.Size = new System.Drawing.Size(133, 199);
+            this.lstKSuMiktari.TabIndex = 4;
+            // 
+            // lstFaturaTutari
+            // 
+            this.lstFaturaTutari.FormattingEnabled = true;
+            this.lstFaturaTutari.Location = new System.Drawing.Point(359, 239);
+            this.lstFaturaTutari.Name = "lstFaturaTutari";
+            this.lstFaturaTutari.Size = new System.Drawing.Size(133, 199);
+            this.lstFaturaTutari.TabIndex = 4;
+            // 
+            // radioMesken
+            // 
+            this.radioMesken.AutoSize = true;
+            this.radioMesken.Location = new System.Drawing.Point(69, 19);
+            this.radioMesken.Name = "radioMesken";
+            this.radioMesken.Size = new System.Drawing.Size(63, 17);
+            this.radioMesken.TabIndex = 2;
+            this.radioMesken.TabStop = true;
+            this.radioMesken.Text = "Mesken";
+            this.radioMesken.UseVisualStyleBackColor = true;
+            // 
+            // txtSonIndex
+            // 
+            this.txtSonIndex.Location = new System.Drawing.Point(110, 45);
+            this.txtSonIndex.Name = "txtSonIndex";
+            this.txtSonIndex.Size = new System.Drawing.Size(382, 20);
+            this.txtSonIndex.TabIndex = 1;
+            // 
+            // txtIlkIndex
+            // 
+            this.txtIlkIndex.Location = new System.Drawing.Point(110, 71);
+            this.txtIlkIndex.Name = "txtIlkIndex";
+            this.txtIlkIndex.Size = new System.Drawing.Size(382, 20);
+            this.txtIlkIndex.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ad Soyad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(194, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Kul. Su Miktarı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(359, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Fatura Tutarı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lstFaturaTutari);
+            this.Controls.Add(this.lstKSuMiktari);
+            this.Controls.Add(this.lstAdSoyad);
+            this.Controls.Add(this.txtIlkIndex);
+            this.Controls.Add(this.txtSonIndex);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Su Faturası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.RadioButton radioUmumi;
+        private System.Windows.Forms.ListBox lstAdSoyad;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioMesken;
+        private System.Windows.Forms.ListBox lstKSuMiktari;
+        private System.Windows.Forms.ListBox lstFaturaTutari;
+        private System.Windows.Forms.TextBox txtSonIndex;
+        private System.Windows.Forms.TextBox txtIlkIndex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SuFaturasi/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuFaturasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioUmumi.Checked = true;
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lstAdSoyad.Items.Add(txtAdSoyad.Text);
+                double kullanilanSu = Convert.ToDouble(txtSonIndex.Text) - (Convert.ToDouble(txtIlkIndex.Text));
+                
+                double fatura = 0;
+                if (radioMesken.Checked)
+                {
+                    lstKSuMiktari.Items.Add(kullanilanSu);
+                    fatura = kullanilanSu * 0.7 + kullanilanSu * 0.3 * 0.2;
+                    lstFaturaTutari.Items.Add(fatura);
+                }
+                else if (radioUmumi.Checked)
+                {
+                    lstKSuMiktari.Items.Add(kullanilanSu);
+                    fatura = kullanilanSu * 0.3;
+                    lstFaturaTutari.Items.Add(fatura);
+                }
+            }
+            catch (Exception ex)
+            {
+                lstAdSoyad.Items.RemoveAt(lstAdSoyad.Items.Count - 1);
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
